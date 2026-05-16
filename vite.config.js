35047881b61c245626100d04a7be5f5ac805228e
Mitesh <miteshmishra89@gmail.com>
2026-05-16 18:16:32 +0530

    chore: update production build assets for Corporate product

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,43 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/corporate/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss()
   ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/retail': {
+        target: 'http://localhost:3006',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/crm': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/marketing': {
+        target: 'http://localhost:3002',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/booking': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/it': {
+        target: 'http://localhost:3004',
+        changeOrigin: true,
+        ws: true,
+      }
+    }
+  }
 })
